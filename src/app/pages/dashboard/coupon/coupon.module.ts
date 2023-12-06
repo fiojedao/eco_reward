36f127142ef6852132fb77fc50726ca07bdade6f
fiojeda <felix@centaurosolutions.com-at-191127002560>
2023-12-05 21:21:18 -0600

    detalle cupon

--- src/app/pages/dashboard/coupon/coupon.module.ts
+++ src/app/pages/dashboard/coupon/coupon.module.ts
@@ -7,6 +7,21 @@ import { CouponFormComponent } from './coupon-form/coupon-form.component';
 import { MaterialImports } from 'src/app/materialImports.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgxDropzoneModule } from 'ngx-dropzone';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 /* import {} from '@angular/'; */
 
 @NgModule({
@@ -15,7 +30,27 @@ import { NgxDropzoneModule } from 'ngx-dropzone';
     CouponDetailComponent,
     CouponFormComponent,
   ],
-  imports: [CommonModule, MaterialImports, ReactiveFormsModule, NgxDropzoneModule ],
+  imports: [
+    CommonModule, 
+    MaterialImports, 
+    ReactiveFormsModule, 
+    NgxDropzoneModule,
+    MatGridListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    MatCheckboxModule],
   providers: [DatePipe],
 })
 export class CouponModule {}
